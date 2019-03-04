49bb528a8903a34d1bbd69f4e972b7fa942a7210
Wu Haotian <whtsky@gmail.com>
2019-03-04 21:20:56 +0800

    show last 30 logs

--- apps/View/Logs.py
+++ apps/View/Logs.py
@@ -22,9 +22,9 @@ class Mylogs(BaseHandler):
     def GET(self):
         user = self.getcurrentuser()
         try:
-            mylogs = DeliverLog.all().filter("username = ", user.name).order('-time').fetch(limit=10)
+            mylogs = DeliverLog.all().filter("username = ", user.name).order('-time').fetch(limit=30)
         except NeedIndexError: #很多人不会部署，经常出现没有建立索引的情况，干脆碰到这种情况直接消耗CPU时间自己排序得了
-            mylogs = sorted(DeliverLog.all().filter("username = ", user.name), key=attrgetter('time'), reverse=True)[:10]
+            mylogs = sorted(DeliverLog.all().filter("username = ", user.name), key=attrgetter('time'), reverse=True)[:30]
             
         #其他用户的推送记录
         logs = {}
