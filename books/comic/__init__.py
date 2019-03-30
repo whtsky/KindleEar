323be819582d14243e60c341eecab05148244993
Wu Haotian <whtsky@gmail.com>
2019-03-30 22:30:55 +0800

    add support for manhuaren & dm5

--- books/comic/__init__.py
+++ books/comic/__init__.py
@@ -3,6 +3,7 @@ import itertools
 from .cartoonmadbase import CartoonMadBaseBook
 from .tencentbase import TencentBaseBook
 from .manhuaguibase import ManHuaGuiBaseBook
+from .manhuarenbase import ManHuaRenBaseBook
 from .seven33sobase import Seven33SoBaseBook
 from .tohomhbase import ToHoMHBaseBook
 from .dmzjbase import DMZJBaseBook
@@ -12,6 +13,7 @@ ComicBaseClasses = [
     CartoonMadBaseBook,
     TencentBaseBook,
     ManHuaGuiBaseBook,
+    ManHuaRenBaseBook,
     Seven33SoBaseBook,
     ToHoMHBaseBook,
     DMZJBaseBook,
